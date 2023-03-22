2e938d9730e28e06096cf69955400db4693bd980
NishanChamara <nishanchamara.ncb@gmail.com>
2023-03-22 12:55:30 +0530

    sg update

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "web" {
 module "Blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog_new"
+  name    = "Blog_sg"
 
   vpc_id  = module.vpc.public_subnets[0]
 
