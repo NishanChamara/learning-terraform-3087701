c4034f0814855f3855fd95b71a1af46d000b0c54
NishanChamara <nishanchamara.ncb@gmail.com>
2023-03-22 13:33:01 +0530

    h

--- main.tf
+++ main.tf
@@ -35,8 +35,8 @@ module "vpc" {
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog_sg"
-
+  
+  name    = "blog"
   vpc_id  = module.vpc.public_subnets[0]
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
@@ -51,7 +51,7 @@ resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  vpc_security_group_ids = module.blog_sg.security_group_id
 
   tags = {
     Name = "HelloWorld"
